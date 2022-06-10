5de02d68b39f3ed372bd4dcf8b1511e5b1209ad0
Artur Ferreto <arturpessuto@gmail.com>
2022-06-10 11:43:53 -0300

    Atualizacao do Repository de Noticias

--- app/Repositories/Models/NewsRepository.php
+++ app/Repositories/Models/NewsRepository.php
@@ -22,8 +22,7 @@ class NewsRepository extends AbstractRepository
         $results = $this->model::where('autor_id', $authorId);
 
         foreach ($orderBy as $key => $value) {
-            if (strstr($key, '-'))
-            {
+            if (strstr($key, '-')) {
                 $key = substr($key, 1);
             }
 
@@ -35,5 +34,66 @@ class NewsRepository extends AbstractRepository
                 'order_by' => implode(',', array_keys($orderBy)),
                 'limit' => $limit
             ])->toArray();
+    }
+
+    /**
+     * @param string $param
+     * @return array
+     */
+    public function findBy(string $param): array
+    {
+        $query = $this->model::query();
+
+        if(is_numeric($param)) {
+            $news = $query->findOrFail($param);
+        } else {
+            $news = $query->where('slug', $param)->get();
+        }
+
+        return $news->toArray();
+    }
+
+    /**
+     * @param string $param
+     * @param array $data
+     * @return bool
+     */
+    public function editBy(string $param, array $data): bool
+    {
+        if (is_numeric($param)) {
+            $news = $this->model::find($param);
+        } else {
+            $news = $this->model::where('slug', $param);
+        }
+
+        return $news->update($data) ? true : false;
+    }
+
+    /**
+     * @param string $param
+     * @return bool
+     */
+    public function deleteBy(string $param): bool
+    {
+        if(is_numeric($param)) {
+            $news = $this->model::destroy($param);
+        } else {
+            $news = $this->model::where('slug', $param)->delete();
+        }
+
+        return $news ? true : false;
+    }
+
+    /**
+     * Deleta o Autor da notícia pelo ID.
+     *
+     * @param int $authorId
+     * @return bool
+     */
+    public function deleteByAuthor(int $authorId): bool
+    {
+        $news = $this->model::where('autor_id', $authorId)->delete();
+
+        return $news ? true : false;
     }
 }
