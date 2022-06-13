abed9d273ea0811dfaf3c81601bd413395e50bb6
Artur Ferreto <arturpessuto@gmail.com>
2022-06-13 11:02:46 -0300

    Comentando código e criando regras de validação pelo middleware

--- app/Models/ImageNews.php
+++ app/Models/ImageNews.php
@@ -5,16 +5,35 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * Class ImageNews
+ * @package App\Models
+ */
 class ImageNews extends Model
 {
     use SoftDeletes;
 
+    /**
+     * @var string
+     */
     protected $table = 'imagens_noticias';
 
+     /**
+     * @var string[]
+     */
     protected $fillable = [
         'noticia_id',
         'imagem',
         'descricao',
         'ativo'
+    ];
+
+    /**
+     * @var array
+     */
+    public array $rules = [
+        'noticia_id' => 'required|numeric',
+        'imagem' => 'required',
+        'descricao' => 'required|min:10|max:255'
     ];
 }
