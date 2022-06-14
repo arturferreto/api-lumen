875dd63c9bef7687be4ecfa76e86dcba28b30787
Artur Ferreto <arturpessuto@gmail.com>
2022-06-14 11:00:07 -0300

    Criação de slug automática em NewsService

--- app/Services/Specific/NewsService.php
+++ app/Services/Specific/NewsService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Specific;
 
+use Illuminate\Support\Str;
 use App\Services\AbstractService;
 
 /**
@@ -46,5 +47,30 @@ class NewsService extends AbstractService
     public function deleteByAuthor(int $authorId): bool
     {
         return $this->repository->deleteByAuthor($authorId);
+    }
+
+    /**
+     * @param array $data
+     * @return array
+     */
+    public function create(array $data): array
+    {
+        $data['slug'] = Str::slug($data['titulo'] . ' ' . $data['subtitulo']);
+
+        return $this->repository->create($data);
+    }
+
+    /**
+     * @param string $param
+     * @param array $data
+     * @return bool
+     */
+    public function editBy(string $param, array $data): bool
+    {
+        if (isset($data['titulo']) && isset($data['subtitulo'])) {
+            $data['slug'] = Str::slug($data['titulo'] . ' ' . $data['subtitulo']);
+        }
+
+        return $this->repository->editBy($param, $data);
     }
 }
