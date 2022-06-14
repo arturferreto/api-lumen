16aee92a940defb2c7dab6b4bcb883b25d5d5f79
Artur Ferreto <arturpessuto@gmail.com>
2022-06-14 13:59:06 -0300

    Retirando group e adicionando middleware nas rotas post e put

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-use App\Http\Middleware\ValidateDataMiddleware;
 use App\Models\{Author, ImageNews, News};
 
 /*
@@ -20,32 +19,30 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['middleware' => ValidateDataMiddleware::class], function() use ($router) {
-    $router->group(['prefix' => 'api/v1', 'namespace' => 'V1', 'as' => Author::class], function() use ($router) {
-        $router->post('/autores', ['uses' => 'AuthorController@create']);
-        $router->get('/autores', ['uses' => 'AuthorController@findAll']);
-        $router->get('/autores/{id}', ['uses' => 'AuthorController@findOneBy']);
-        $router->put('/autores/{param}', ['uses' => 'AuthorController@editBy']);
-        $router->delete('/autores/{id}', ['uses' => 'AuthorController@delete']);
-    });
+$router->group(['prefix' => 'api/v1', 'namespace' => 'V1', 'as' => Author::class], function() use ($router) {
+    $router->get('/autores', ['uses' => 'AuthorController@findAll']);
+    $router->get('/autores/{id}', ['uses' => 'AuthorController@findOneBy']);
+    $router->post('/autores', ['uses' => 'AuthorController@create', 'middleware' => 'ValidateDataMiddleware']);
+    $router->put('/autores/{param}', ['uses' => 'AuthorController@editBy', 'middleware' => 'ValidateDataMiddleware']);
+    $router->delete('/autores/{id}', ['uses' => 'AuthorController@delete']);
+});
 
-    $router->group(['prefix' => 'api/v1', 'namespace' => 'V1', 'as' => News::class], function() use ($router) {
-        $router->post('/noticias', ['uses' => 'NewsController@create']);
-        $router->get('/noticias', ['uses' => 'NewsController@findAll']);
-        $router->get('/noticias/autor/{author}', ['uses' => 'NewsController@findByAuthor']);
-        $router->get('/noticias/{param}', ['uses' => 'NewsController@findBy']);
-        $router->put('/noticias/{param}', ['uses' => 'NewsController@editBy']);
-        $router->delete('/noticias/{param}', ['uses' => 'NewsController@deleteBy']);
-        $router->delete('/noticias/autor/{author}', ['uses' => 'NewsController@deleteByAuthor']);
-    });
+$router->group(['prefix' => 'api/v1', 'namespace' => 'V1', 'as' => News::class], function() use ($router) {
+    $router->get('/noticias', ['uses' => 'NewsController@findAll']);
+    $router->get('/noticias/autor/{author}', ['uses' => 'NewsController@findByAuthor']);
+    $router->get('/noticias/{param}', ['uses' => 'NewsController@findBy']);
+    $router->post('/noticias', ['uses' => 'NewsController@create', 'middleware' => 'ValidateDataMiddleware']);
+    $router->put('/noticias/{param}', ['uses' => 'NewsController@editBy', 'middleware' => 'ValidateDataMiddleware']);
+    $router->delete('/noticias/{param}', ['uses' => 'NewsController@deleteBy']);
+    $router->delete('/noticias/autor/{author}', ['uses' => 'NewsController@deleteByAuthor']);
+});
 
-    $router->group(['prefix' => 'api/v1', 'namespace' => 'V1', 'as' => ImageNews::class], function() use ($router) {
-        $router->post('/imagens-noticias', ['uses' => 'ImageNewsController@create']);
-        $router->get('/imagens-noticias', ['uses' => 'ImageNewsController@findAll']);
-        $router->get('/imagens-noticias/{id}', ['uses' => 'ImageNewsController@findOneBy']);
-        $router->get('/imagens-noticias/noticia/{news}', ['uses' => 'ImageNewsController@findByNews']);
-        $router->put('/imagens-noticias/{param}', ['uses' => 'ImageNewsController@editBy']);
-        $router->delete('/imagens-noticias/{id}', ['uses' => 'ImageNewsController@delete']);
-        $router->delete('/imagens-noticias/noticia/{news}', ['uses' => 'ImageNewsController@deleteByNews']);
-    });
+$router->group(['prefix' => 'api/v1', 'namespace' => 'V1', 'as' => ImageNews::class], function() use ($router) {
+    $router->get('/imagens-noticias', ['uses' => 'ImageNewsController@findAll']);
+    $router->get('/imagens-noticias/{id}', ['uses' => 'ImageNewsController@findOneBy']);
+    $router->get('/imagens-noticias/noticia/{news}', ['uses' => 'ImageNewsController@findByNews']);
+    $router->post('/imagens-noticias', ['uses' => 'ImageNewsController@create', 'middleware' => 'ValidateDataMiddleware']);
+    $router->put('/imagens-noticias/{param}', ['uses' => 'ImageNewsController@editBy', 'middleware' => 'ValidateDataMiddleware']);
+    $router->delete('/imagens-noticias/{id}', ['uses' => 'ImageNewsController@delete']);
+    $router->delete('/imagens-noticias/noticia/{news}', ['uses' => 'ImageNewsController@deleteByNews']);
 });
