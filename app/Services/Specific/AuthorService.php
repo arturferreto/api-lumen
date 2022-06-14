f67f491dd722c9e3c840fcbb25e80f93bfb56b98
Artur Ferreto <arturpessuto@gmail.com>
2022-06-14 09:41:32 -0300

    Criptografando a senha no Author Service

--- app/Services/Specific/AuthorService.php
+++ app/Services/Specific/AuthorService.php
@@ -10,5 +10,28 @@ use App\Services\AbstractService;
  */
 class AuthorService extends AbstractService
 {
-    //
+    /**
+     * @param array $data
+     * @return array
+     */
+    public function create(array $data): array
+    {
+        $data['senha'] = encrypt($data['senha']);
+
+        return $this->repository->create($data);
+    }
+
+    /**
+     * @param string $param
+     * @param array $data
+     * @return bool
+     */
+    public function editBy(string $param, array $data): bool
+    {
+        if (isset($data['senha'])) {
+            $data['senha'] = encrypt($data['senha']);
+        }
+
+        return $this->repository->editBy($param, $data);
+    }
 }
